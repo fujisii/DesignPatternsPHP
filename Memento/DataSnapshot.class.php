8f1c8aec1dd2560c2b379f21d0724e15e26b2376
fujisii <51264835+fujisii@users.noreply.github.com>
2020-06-16 00:33:42 +0900

    add Memento

---
+++ Memento/DataSnapshot.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Mementoに相当する
+ */
+class DataSnapshot
+{
+
+    private $comment;
+
+    protected function __construct($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    protected function getComment()
+    {
+        return $this->comment;
+    }
+}
