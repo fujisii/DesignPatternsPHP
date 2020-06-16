4268d8c6d835bc113160912b9e0fa3a24141e173
fujisii <51264835+fujisii@users.noreply.github.com>
2020-06-16 22:50:09 +0900

    add Observer

---
+++ Observer/LoggingListener.class.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'CartListener.class.php';
+
+/**
+ * ConcreteObserverクラスに相当する
+ */
+class LoggingListener implements CartListener
+{
+
+    public function __construct()
+    {
+    }
+
+    public function update(Cart $cart)
+    {
+        echo '<pre>';
+        var_dump($cart->getItems());
+        echo '</pre>';
+    }
+}
