4d6e89902980687813df4d220450e16872a8d6e8
fujisii <51264835+fujisii@users.noreply.github.com>
2020-06-20 14:15:28 +0900

    add State

---
+++ State/UserState.class.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Stateクラスに相当する
+ * 状態毎の動作・振る舞いを定義する
+ */
+interface UserState {
+    public function isAuthenticated();
+    public function nextState();
+    public function getMenu();
+}
